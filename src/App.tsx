1767fcc069c260fc8e2eb4d48203b95863464299
dandpanda <dan.v.dinh@gmail.com>
2022-05-02 16:41:31 -0500

    Adding back footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Mari from './components/Mari';
 
@@ -7,6 +8,7 @@ function App() {
     <body>
       <Header />
       <Mari />
+      <Footer />
     </body>
   );
 }
