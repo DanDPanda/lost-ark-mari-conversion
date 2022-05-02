4c1bc1b16e4eac5290b38feb6ac0a3be3b92883e
dandpanda <dan.v.dinh@gmail.com>
2022-05-02 16:37:09 -0500

    Increase icon size and made the background extend all the way to the bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import './App.css';
-import Footer from './components/Footer';
 import Header from './components/Header';
 import Mari from './components/Mari';
 
-var rootStyle = {
-  background: '#131924',
-  color: '#131924',
-  height: '100%'
-}
-
 function App() {
   return (
-    <div style={rootStyle}>
+    <body>
       <Header />
       <Mari />
-      <Footer />
-    </div>
+    </body>
   );
 }
 
